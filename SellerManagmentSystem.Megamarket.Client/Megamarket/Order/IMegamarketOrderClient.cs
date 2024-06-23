c587894539d6e94fd5e3a4683e251c408973b87d
aboykov <boykov.rtm@gmail.com>
2024-06-23 19:35:20 +0300

    init asp net application

---
+++ SellerManagmentSystem.Megamarket.Client/Megamarket/Order/IMegamarketOrderClient.cs
@@ -0,0 +1,10 @@
+﻿using Refit;
+
+namespace SellerManagmentSystem.Megamarket.Client;
+
+
+public interface IMegamarketOrderClient
+{
+    [Post("order/search")]
+    Task<Search.Response> Search(Search.Request request, CancellationToken cancellationToken);
+}
---
+++ SellerManagmentSystem.Megamarket.Client/Megamarket/Order/Search.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SellerManagmentSystem.Megamarket.Client;
+
+public sealed record Search
+{
+    public sealed record Request
+    {
+        public sealed record Data
+        {
+            [JsonPropertyName("token")] public string Token { get; init; }
+        }
+
+        public sealed record Meta
+        {
+
+        }
+    }
+
+
+    public sealed record Response
+        {
+            
+        }
+}
